4e964fe07e55fa2f55a285140e2a024fc8c9b3c8
Paul Worrall <paul.worrall@interition.net>
2018-02-09 09:54:03 +0000

    changed from 8 decimal to 18 decimal as per recommended standard

--- contracts/TogetherToken.sol
+++ contracts/TogetherToken.sol
@@ -5,5 +5,5 @@ import 'zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
 contract TogetherToken is MintableToken {
   string public name = "Together Token";
   string public symbol = "TOG";
-  uint8 public decimals = 8;
+  uint8 public decimals = 18; // this is the recommended decimals
 }
